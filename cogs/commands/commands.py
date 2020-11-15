7eb387937b4a01ba276877beb5b78c1c490ebec7
Milo Weinberg <38477514+Iapetus-11@users.noreply.github.com>
2020-11-14 23:33:21 -0500

    Add "say" command

--- cogs/commands/commands.py
+++ cogs/commands/commands.py
@@ -5,6 +5,12 @@ class Commands(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        
+    @command.command(name='say')
+    async def say_something(self, ctx, *, text):
+        """Bot repeats what you put into the command"""
+        
+        await ctx.send(text)
 
 
 def setup(bot):
